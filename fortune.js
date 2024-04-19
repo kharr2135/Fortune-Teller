e433c58c3c0cdbfebd5600e699fd646aee747968
Kharr2135 <kianharry123@gmail.com>
2024-04-19 16:07:28 -0700

    it works

--- fortune.js
+++ fortune.js
@@ -2,8 +2,20 @@
 //returns a random fortune to the user using JS!
 const fortunes = ['you are going to be very lucky in the near future', 'you are going to get a new car sometime tomorrow', 
                     'you are going to die alone', 'you will become a bakabear tomorrow', 'they are after you', 'do not go outside tomorrow'] 
-            
-const fortuneTeller = name => {
-    return name + 'this is your fortune' + fortunes.random()
-}
-console.log(fortuneTeller)
+
+                    const generateRandomIndex = (max) => {
+                        return Math.floor(Math.random() * max);
+                      };
+                      
+                      const generateFortune = () => {
+                        const index = generateRandomIndex(fortunes.length);
+                        return fortunes[index];
+                      };
+                      
+                      const showFortune = () => {
+                        const fortune = generateFortune();
+                        console.log("Your fortune for today is:");
+                        console.log(fortune);
+                      };
+
+showFortune()
