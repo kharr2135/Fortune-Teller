5b03e10746a56abab8030e161778b10274386653
Kharr2135 <kianharry123@gmail.com>
2024-04-19 16:01:00 -0700

    created v1

--- fortune.js
+++ fortune.js
@@ -1,4 +1,9 @@
 //fortune teller
 //returns a random fortune to the user using JS!
 const fortunes = ['you are going to be very lucky in the near future', 'you are going to get a new car sometime tomorrow', 
-                    'you are going to die alone', 'you will become a bakabear tomorrow', 'they are after you']
+                    'you are going to die alone', 'you will become a bakabear tomorrow', 'they are after you', 'do not go outside tomorrow'] 
+            
+const fortuneTeller = name => {
+    return name + 'this is your fortune' + fortunes.random()
+}
+console.log(fortuneTeller)
