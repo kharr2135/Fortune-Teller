4da4c0cda184c987c6fddd494e85f3f8ba34bfba
Kharr2135 <kianharry123@gmail.com>
2024-04-19 15:54:54 -0700

    first outline

---
+++ fortune.js
@@ -0,0 +1,4 @@
+//fortune teller
+//returns a random fortune to the user using JS!
+const fortunes = ['you are going to be very lucky in the near future', 'you are going to get a new car sometime tomorrow', 
+                    'you are going to die alone', 'you will become a bakabear tomorrow', 'they are after you']
